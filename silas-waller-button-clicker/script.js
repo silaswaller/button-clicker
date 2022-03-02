f7288f9556d9c5ea9bf80f517db006b7164da1c9
Silas Waller <97419274+silaswaller@users.noreply.github.com>
2022-03-01 19:06:38 -0500

    Add files via upload

---
+++ silas-waller-button-clicker/script.js
@@ -0,0 +1,13 @@
+function myFunction(){
+    console.log("Hello World");
+}
+
+function changeColor(element){
+    element.style.color = "red";
+}
+
+function like(id){
+    document.querySelector(id).innerText++;
+}
+
+changeColor(h1);
